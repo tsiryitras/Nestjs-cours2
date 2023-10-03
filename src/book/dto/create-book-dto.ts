cbaf76feb94c16641f7908c4ad9f9efa6854cf2d
tsirinirinaitras <tsirinirina.rajaonarison@dreamslab.fr>
2023-10-03 14:36:24 +0300

    Pipe validation

--- src/book/dto/create-book-dto.ts
+++ src/book/dto/create-book-dto.ts
@@ -1,9 +1,27 @@
+import { IsEnum, IsNotEmpty, IsNumber, IsString } from 'class-validator';
 import { Category } from '../entities/book.schema';
 
 export class CreateBookDto {
+  /**
+   * une titre doit être une chaîne
+   */
+  @IsNotEmpty()
+  @IsString()
   readonly title: string;
+
+  @IsNotEmpty()
+  @IsString()
   readonly description: string;
+
+  @IsNotEmpty()
+  @IsString()
   readonly author: string;
+
+  @IsNotEmpty()
+  @IsNumber()
   readonly price: number;
+
+  @IsNotEmpty()
+  @IsEnum(Category, { message: 'Please enter correct category' })
   readonly category: Category;
 }
--- src/book/dto/update-book-dto.ts
+++ src/book/dto/update-book-dto.ts
@@ -1,9 +1,24 @@
+import { IsEnum, IsNumber, IsOptional, IsString } from 'class-validator';
 import { Category } from '../entities/book.schema';
 
 export class UpdateBookDto {
+  @IsString()
+  @IsOptional()
   readonly title: string;
+
+  @IsString()
+  @IsOptional()
   readonly description: string;
+
+  @IsString()
+  @IsOptional()
   readonly author: string;
+
+  @IsNumber()
+  @IsOptional()
   readonly price: number;
+
+  @IsEnum(Category, { message: 'Please enter correct category.' })
+  @IsOptional()
   readonly category: Category;
 }
